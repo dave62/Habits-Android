83e77729d2e53379e6ea89a1f9b50d719f898cbb
David Sauvage <davidsauvage8@gmail.com>
2017-08-17 10:43:34 +0200

    Add tabs to the DayRecordActivity
     -> First tab : calendar
     -> Second tab : statistics (TODO)

--- app/src/main/java/com/github/dave62/habits/activity/DayRecordActivity.java
+++ app/src/main/java/com/github/dave62/habits/activity/DayRecordActivity.java
@@ -3,6 +3,7 @@ package com.github.dave62.habits.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CalendarView;
+import android.widget.TabHost;
 
 import com.github.dave62.habits.R;
 import com.github.dave62.habits.model.Habit;
@@ -19,6 +20,8 @@ public class DayRecordActivity extends AppCompatActivity {
 
     @ViewById
     protected CalendarView calendarView;
+    @ViewById
+    protected TabHost tabHost;
 
     @Extra
     protected String currentHabitId;
@@ -34,7 +37,28 @@ public class DayRecordActivity extends AppCompatActivity {
 
     @AfterViews
     protected void afterViews() {
+
+        initializeTabs();
+
+        //TODO : Handle async request ?
         currentHabit = realm.where(Habit.class).equalTo("id", currentHabitId).findFirst();
         calendarView.setMinDate(currentHabit.getStartingDate().getTime());
+        calendarView.set
+    }
+
+    private void initializeTabs() {
+        tabHost.setup();
+
+        //Calendar Tab
+        TabHost.TabSpec spec = tabHost.newTabSpec("Calendar");
+        spec.setContent(R.id.calendarTab);
+        spec.setIndicator("Calendar");
+        tabHost.addTab(spec);
+
+        //Statistics Tab
+        spec = tabHost.newTabSpec("Stats");
+        spec.setContent(R.id.statisticsTab);
+        spec.setIndicator("Statistics");
+        tabHost.addTab(spec);
     }
 }
